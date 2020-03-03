70d5fe8616fe23e44752abcf312d202a29755f44
John Ko <jko121790@gmail.com>
2020-03-03 15:36:16 -0800

    memory match feature 3 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var mainElement = document.getElementById("gameCards");
-
 mainElement.addEventListener("click", handleClick);
+
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
 
 function handleClick(event) {
   if(event.target.className.indexOf("card-back") === -1) {
@@ -8,5 +12,28 @@ function handleClick(event) {
   }
   var targetElement = event.target;
   targetElement.classList.add("hidden");
-  console.log("event: ", event);
+
+  if (!firstCardClicked) {
+    firstCardClicked = event.target;
+    firstCardClasses = event.target.previousElementSibling.className;
+  } else {
+    secondCardClicked = event.target;
+    secondCardClasses = event.target.previousElementSibling.className;
+    mainElement.removeEventListener("click", handleClick);
+    if(firstCardClasses === secondCardClasses) {
+      mainElement.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      setTimeout(hideCard, 1500);
+    }
+  }
+}
+
+function hideCard() {
+  firstCardClicked.classList.remove("hidden");
+  secondCardClicked.classList.remove("hidden");
+  firstCardClicked = null;
+  secondCardClicked = null;
+  mainElement.addEventListener("click", handleClick);
 }
