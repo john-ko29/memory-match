cd1cc5d77eaab2ce77162481648dea41200c7eb2
John Ko <jko121790@gmail.com>
2020-03-03 14:37:58 -0800

    Memory match complete handleClick function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+var mainElement = document.getElementById("gameCards");
 
+mainElement.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  if(event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  var targetElement = event.target;
+  targetElement.classList.add("hidden");
+  console.log("event: ", event);
+}
