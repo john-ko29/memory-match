cbbfb111e56767ac9fac6b8eb76df31ebb0378d6
John Ko <jko121790@gmail.com>
2020-03-03 17:32:35 -0800

    memory match feature 6 reset game complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var modalElement = document.getElementById("modal");
 var gamePlayedElement = document.getElementById("gamePlayed");
 var attemptsElement = document.getElementById("attempts");
 var accuracyElement = document.getElementById("accuracy");
+var buttonElement = document.getElementById("resetGame");
+buttonElement.addEventListener("click", resetGame);
 
 var firstCardClicked;
 var secondCardClicked;
@@ -15,6 +17,7 @@ var matches = 0;
 
 var attempts = 0;
 var gamesPlayed = 0;
+displayStats();
 
 function handleClick(event) {
   if(event.target.className.indexOf("card-back") === -1) {
@@ -63,5 +66,25 @@ function displayStats() {
 }
 
 function calculateAccuracy(attempt, match){
+  if(attempt === 0) {
+    return 0 + "%";
+  }
   return Math.trunc((match / attempt) * 100) + "%";
+}
+
+function resetGame() {
+  matches = 0;
+  attempts = 0;
+  gamesPlayed++;
+
+  displayStats();
+  resetCards();
+  modalElement.classList.add("hidden");
+}
+
+function resetCards() {
+  var hiddenCards = document.querySelectorAll(".card-back");
+  for(var index = 0; index < hiddenCards.length; index++) {
+    hiddenCards[index].classList.remove("hidden");
+  }
 }
